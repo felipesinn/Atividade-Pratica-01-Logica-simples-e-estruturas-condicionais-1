f0a2d50383374852bf8ae2ef118280fb783d1aaa
felipesinn <felipesinn01@gmail.com>
2023-08-06 23:50:53 -0300

    exercicio 4 ok

---
+++ exer4/script.js
@@ -0,0 +1,10 @@
+let minutos = 120;
+let totalSegudos = minutos * 60;
+    document.write(`${minutos} minutos é equivale à ${totalSegudos} segundos!`)
+
+
+// 4. Crie duas variáveis. Na primeira coloque um total de minutos e
+// defina um valor para ela (por exemplo, minutos = 120). Na segunda
+// coloque o total em segundos destes minutos armazenados na
+// primeira variável. Apresente no documento html a seguinte
+// informação: "_ minutos equivale à _ segundos!"
