4332f8e3aa05e0da042b694f4dd7b9f2f158c2ee
felipesinn <felipesinn01@gmail.com>
2023-08-06 22:56:24 -0300

    exercicio 1 ok

---
+++ exer1/script.js
@@ -0,0 +1,8 @@
+let idade =Number(prompt("Digite sua idade"));
+    document.write(`Minha idade é ${idade} anos`);
+
+
+// 1. Crie uma variável JavaScript e coloque nela o valor de sua idade.
+// Mostre no html usando o document.write() o dado contido na
+// variável junto da frase "Minha idade é x anos", sendo "x" o valor
+// armazenado na sua variável.
