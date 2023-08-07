fef2d20b871951c0428398eaad9dc0b2e7b496b1
felipesinn <felipesinn01@gmail.com>
2023-08-06 23:35:13 -0300

    exercicio 3 ok

---
+++ exer3/script.js
@@ -0,0 +1,13 @@
+let totaCompra = 149.90;
+let parcelas = 2;
+let soma = totaCompra / parcelas;
+    document.write(`O valor total da compra foi R$ ${totaCompra} </p> Forma de pagamento: ${parcelas} de ${soma}`)
+
+
+// 3. Crie três variáveis, na primeira variável coloque o total de uma
+// compra, por exemplo 149.90. Na segunda variável coloque a
+// quantidade de parcelas, por exemplo 2. Na terceira variável coloque
+// o valor da parcela. Apresente no documento html as seguintes
+// informações:
+// "O valor total da compra foi R$_,_"
+// "Forma de pagamento: _x de R$_,_"
