b261d7f2a9a043b1e94a2d4c3db48a84167e9cb3
felipesinn <felipesinn01@gmail.com>
2023-08-06 23:26:31 -0300

    exercicio 2 ok

---
+++ exer2/script.js
@@ -0,0 +1,10 @@
+// Crie três variáveis JavaScript, em duas delas atribua os valores que
+// você quiser e na outra atribua o valor da soma das duas primeiras
+// variáveis. Apresente valor da soma no documento html junto da
+// frase "A resultado da soma de x e y é z", sendo x o valor armazenado
+// na primeira variável, y o valor armazenado segunda variável e z o
+// valor armazenado na terceira variável
+let valor1 = 10;
+let valor2 = 5;
+let soma = valor1 + valor2 ;
+document.write(`O resultado da soma do valor1 + valor2 é ${soma}`)
