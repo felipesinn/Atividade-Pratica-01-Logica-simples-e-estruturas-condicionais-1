62e15f00b42ebffed2a67fd2707e4fad272646f0
felipesinn <felipesinn01@gmail.com>
2023-08-07 00:06:49 -0300

    exercicio 5 ok

---
+++ exer5/script.js
@@ -0,0 +1,12 @@
+let aluno ="Jõao Pedro"
+let nota1 = 8;
+let nota2 = 9;
+let nota3 = 6;
+let somasNotas =(nota1 + nota2 + nota3) /3;
+    document.write(`O aluno ${aluno} ficou com média ${somasNotas}`)
+
+// 5. Crie cinco variáveis. Na primeira armazene o nome de um aluno. Na
+// segunda, terceira e quarta coloque 3 notas (valores de 0 à 10).
+// Calcule a média das notas e armazene na quinta variável criada.
+// Apresente no documento html a seguinte informação:
+// "O aluno _____ ficou com média _,_"
